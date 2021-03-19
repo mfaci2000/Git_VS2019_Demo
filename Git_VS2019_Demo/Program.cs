74e1cf65e5ed2655d67f4ef2f87e205552954f17
Sofiane taleb <str14.don@gmail.com>
2021-03-19 20:12:14 +0100

    Add project files.

---
+++ Git_VS2019_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_VS2019_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
